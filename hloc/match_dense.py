b7331b5f7fe72003378367479906e6b327cafa8f
NguyenTuanMi <ntuanminh0309@gmail.com>
2026-05-28 21:08:35 +0800

    feat-fix(aspanformer): add Aspanformer dependecies and fix pipeline' s recursive issues

--- hloc/match_dense.py
+++ hloc/match_dense.py
@@ -60,21 +60,18 @@ confs = {
         "max_error": 4,  # max error for assigned keypoints (in px)
         "cell_size": 4,  # size of quantization patch (max 1 kp/patch)
     },
-    "aspanformer": {
-        "output": "matches-aspanformer",
-        "model": {
-            "name": "aspanformer",
-            "weights": "outdoor",
-            "max_keypoints": 2000,
-            "match_threshold": 0.2,
+    
+    'aspanformer': {
+        'output': 'matches-aspanformer',
+        'model': {
+            'name': 'aspanformer',
+            'weights': 'outdoor'
         },
-        "preprocessing": {
-            "grayscale": True,
-            "force_resize": True,
-            "resize_max": 1024,
-            "width": 640,
-            "height": 480,
-            "dfactor": 8,
+        'preprocessing': {
+            'grayscale': True,
+            'force_resize': True,
+            'resize_max': 1024,
+            'dfactor': 8
         },
     },
 }
